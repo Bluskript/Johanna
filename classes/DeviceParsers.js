c66b60d2dd020937d168311b592d403177f92f3d
Bluskript <52386117+Bluskript@users.noreply.github.com>
2021-07-17 21:50:20 -0400

    Update DeviceParsers.js

--- classes/DeviceParsers.js
+++ classes/DeviceParsers.js
@@ -4,8 +4,8 @@
  * and they parse the data so they are properly formatted
  * @author George Tsotsos
  */
-function parseDefaults(report) {
-  report.uptime = parseInt(parseInt(report.uptime) / 100);
+ function parseDefaults(report) {
+  report.uptime = +report.uptime / 100;
   return report;
 }
 
@@ -13,32 +13,32 @@ module.exports = {
   undefined: (report) => parseDefaults(report), // If the library is missing just parse the defaults
   "unifi.switch": (report) => {
     report = parseDefaults(report);
-    report.temperature = parseInt(report.temperature);
-    report.fan_status = parseInt(report.fan_status);
+    report.temperature = +report.temperature
+    report.fan_status = +report.fan_status
     report.ram = {
-      free: (parseInt(report.memory_free) / 1000 / 1000).toFixed(2),
-      total: (parseInt(report.memory_total) / 1000 / 1000).toFixed(2),
-      used: ((parseInt(report.memory_total) - parseInt(report.memory_free)) / 1000 / 1000).toFixed(2),
+      free: (+report.memory_free / 1000 / 1000).toFixed(2),
+      total: (+report.memory_total / 1000 / 1000).toFixed(2),
+      used: ((+report.memory_total - +report.memory_free) / 1000 / 1000).toFixed(2),
     };
-    report.cpu = parseInt(report.cpu_usage.split("(")[1].trim().replace("%)", ""));
+    report.cpu = +report.cpu_usage.spli("(")[1].trim().replace("%)", "");
     return report;
   },
   "unifi.ap": (report) => {
     report = parseDefaults(report);
     report.radio = {
-      TxPackets: parseInt(report.radio_tx_packets),
-      RxPackets: parseInt(report.radio_rx_packets),
+      TxPackets: +report.radio_tx_packets,
+      RxPackets: +report.radio_rx_packets
     };
-    report.cpu = parseInt(report.cpu);
+    report.cpu = +report.cpu
 
     return report;
   },
   idrac: (report) => {
     report = parseDefaults(report);
-    report.system_board_inlet_temp = parseInt(report.system_board_inlet_temp) / 10;
-    report.system_board_exhaust_temp = parseInt(report.system_board_exhaust_temp) / 10;
-    report.system_board_power_consumption = parseInt(report.system_board_power_consumption);
-    report.cpus = [{ temp: parseInt(report.cpu1_temp) / 10 }, { temp: parseInt(report.cpu2_temp) / 10 }];
+    report.system_board_inlet_temp = +report.system_board_inlet_temp / 10;
+    report.system_board_exhaust_temp = +report.system_board_exhaust_temp / 10;
+    report.system_board_power_consumption = +report.system_board_power_consumption
+    report.cpus = [{ temp: +report.cpu1_temp / 10 }, { temp: +report.cpu2_temp / 10 }];
     return report;
   },
 };
